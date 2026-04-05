f0976d00fb3233a79982b99cd10120225f01c3a5
trillnjoy <trillnjoy@icloud.com>
2026-04-05 11:51:19 -0700

    Add Pediatric BP Calculator PWA

---
+++ bp-calculator-sw.js
@@ -0,0 +1,42 @@
+// Service Worker for Pediatric BP Calculator PWA
+const CACHE_NAME = 'pediatric-bp-calc-v2.0';
+const urlsToCache = [
+  './pediatric-bp-calculator.html',
+  './bp-calculator-manifest.json',
+  'https://unpkg.com/react@18.2.0/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone@7.23.5/babel.min.js'
+];
+
+// Install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('./pediatric-bp-calculator.html'))
+  );
+});
